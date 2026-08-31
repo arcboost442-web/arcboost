95f29a34a00924a27c19b14e83e6196e33085529
Onat <arcboost442@gmail.com>
2026-08-31 19:36:27 +0700

    feat: middleware redirect to coming soon

---
+++ middleware.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  if (
+    pathname.startsWith("/coming-soon") ||
+    pathname.startsWith("/api") ||
+    pathname.startsWith("/_next") ||
+    pathname.startsWith("/favicon")
+  ) {
+    return NextResponse.next();
+  }
+  return NextResponse.redirect(new URL("/coming-soon", request.url));
+}
+
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+};
